833ff08f44d287f999daecbe57b2794179ef6471
KyoTakushi <takushi@pla-cole.co>
2021-01-12 13:44:06 +0900

    fixed casing problem

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
-import Layout from "../components/Layout"
-import Header from "../components/Header"
+import Layout from "../components/layout"
+import Header from "../components/header"
 import MetaIndex from "../components/meta/meta-index"
 import PortfolioContent from "../components/portfolio/PortfolioContent"
 
